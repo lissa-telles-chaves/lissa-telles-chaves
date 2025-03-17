d79718127641a9d29413f4b755be0f03b41cc883
lissa-telles-chaves <lissatchaves@gmail.com>
2025-03-17 14:47:39 -0400

    Update app.py

--- TELLES_Python_portfolio/app.py
+++ TELLES_Python_portfolio/app.py
@@ -2,8 +2,8 @@
 import streamlit as st
 import pandas as pd
 st.title("MY FIRST APP")
-st.write("welcome to my first app. Here, you will be able to access filtered information on yhe penguins.csv dataset, through their species and island!")
-data = pd.read_csv("/Users/lissachaves/TELLES_PORTFOLIO/basic_streamlit_app/data/penguins.csv")
+st.write("welcome to my first app. Here, you will be able to access filtered information on the penguins.csv dataset, through their species and island!")
+data = pd.read_csv("GitHub/lissa-telles-chaves/TELLES_Python_portfolio/data/penguins.csv")
 species = st.sidebar.selectbox('Select species', data['species'].unique())
 island = st.sidebar.selectbox('Select island', data['island'].unique())
 filtered_data = data[(data['species'] == species) & (data['island'] == island)]
